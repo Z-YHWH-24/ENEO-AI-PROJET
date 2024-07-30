15c0e568cff99a00b8a3466c10219909e2d8043f
Z-YHWH-24 <zenguewarren@gmail.com>
2024-07-30 11:45:17 +0100

    did

--- lib/interfaces/welcome.dart
+++ lib/interfaces/welcome.dart
@@ -1,5 +1,9 @@
+//imported dependencies
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'package:flutter/cupertino.dart';
 
+//class creation
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
@@ -7,6 +11,52 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Color.fromARGB(255, 11, 202, 91),
+    );
+  }
+}
+
+class AnimatedBtn extends StatelessWidget {
+  const AnimatedBtn({
+    Key? key,
+    required RiveAnimationController btnAnimationController,
+    required this.press,
+  })  : _btnAnimationController = btnAnimationController,
+        super(key: key);
+
+  final RiveAnimationController _btnAnimationController;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: SizedBox(
+        height: 64,
+        width: 236,
+        child: Stack(
+          children: [
+            RiveAnimation.asset(
+              "assets/RiveAssets/button.riv",
+              controllers: [_btnAnimationController],
+            ),
+            Positioned.fill(
+              top: 8,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Icon(CupertinoIcons.arrow_right),
+                  const SizedBox(width: 8),
+                  Text(
+                    "Start the course",
+                    style: Theme.of(context).textTheme.labelLarge,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
