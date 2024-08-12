ffd4791e7f6888b41eabac854ef6a6503c90f0f8
Z-YHWH-24 <158236140+Z-YHWH-24@users.noreply.github.com>
2024-08-12 01:24:27 +0000

    project renewal

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:eneo_ai_project/interfaces/interface_one/WelcomePage.dart';
+import 'package:pages/'
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,33 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'The Flutter Way',
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFFEEF1F8),
-        primarySwatch: Colors.blue,
-        fontFamily: "Intel",
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(foregroundColor: Colors.white),
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          filled: true,
-          fillColor: Colors.white,
-          errorStyle: TextStyle(height: 0),
-          border: defaultInputBorder,
-          enabledBorder: defaultInputBorder,
-          focusedBorder: defaultInputBorder,
-          errorBorder: defaultInputBorder,
-        ),
-      ),
-      home: const WelcomeScreen(),
-    );
-  }
-}
-
-const defaultInputBorder = OutlineInputBorder(
-  borderRadius: BorderRadius.all(Radius.circular(16)),
-  borderSide: BorderSide(
-    color: Color(0xFFDEE3F2),
-    width: 1,
-  ),
+      home: WelcomePage(),
+      
 );
