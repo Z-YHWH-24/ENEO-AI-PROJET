9526ad23e07f6a7bb193488aee12c413d6de9368
Audeb414 <ngassamnguepaaudesandra@gmail.com>
2024-08-13 16:14:01 +0100

    ai chat interface done

---
+++ lib/components/My_message_header.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class HeatherSection extends StatefulWidget {
+  const HeatherSection({super.key});
+
+  @override
+  State<HeatherSection> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HeatherSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 350,
+      color: const Color.fromARGB(255, 27, 118, 187),
+      height: 50,
+      padding: const EdgeInsets.only(right: 5),
+      child: Row(
+        children: [
+          //const Padding(padding: EdgeInsets.all(5)),
+          Image.asset(
+            'assets/eneo-logo.png',
+          ),
+          /* Container(
+            height: 70,
+            width: 100,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/eneo-logo.png'),
+              ),
+            ),
+          ),*/
+          const SizedBox(
+            width: 5,
+          ),
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.end, //decaler vers la gauche
+            children: [
+              Padding(padding: EdgeInsets.only(top: 10)),
+              Text(
+                'IA ENEO',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'writing...',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color.fromARGB(255, 140, 198, 64),
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+
+          const Icon(
+            Icons.notifications_active_outlined,
+            size: 20,
+            color: Colors.white,
+          ),
+          const Icon(
+            Icons.settings,
+            size: 20,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/My_message_input.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class SearchSection extends StatefulWidget {
+  const SearchSection({super.key});
+
+  @override
+  State<SearchSection> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<SearchSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      //width: 1000,
+      //height: 100,
+      //color: Colors.amber,
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color.fromARGB(255, 140, 198, 64),
+              ),
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 25,
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.grey.shade200,
+                filled: true,
+                alignLabelWithHint: true,
+                border: OutlineInputBorder(
+                  //Arrondir les bords
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+                hintText: "entrer votre message...",
+                contentPadding: const EdgeInsets.symmetric(vertical: 16),
+                hintStyle: const TextStyle(
+                  textBaseline: TextBaseline.alphabetic,
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color.fromARGB(255, 140, 198, 64),
+              ),
+              child: const Icon(
+                Icons.mic_outlined,
+                color: Colors.white,
+                size: 25,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ); //des fonctionnalites de design avancé
+  }
+}
